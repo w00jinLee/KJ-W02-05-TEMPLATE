f61fe4c936ef08fa3e8102d5942f40daf3c0ad58
w00jinLee <leewoojin001121@gmail.com>
2026-03-08 05:05:35 +0900

    [WEEK2] 정수론 - 소수 찾기

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,26 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+
+
+N = int(input())
+cnt = 0
+numbers = input().split()
+
+for i in range(len(numbers)):
+    numbers[i] = int(numbers[i])
+    
+print(numbers)
+
+def  checkNumber(num):
+    if num == 2:
+        return True
+    for i in range(2, num-1):
+        if num % i == 0:
+            return False
+    return True
+
+for i in range(len(numbers)):
+    if numbers[i] != 1 and checkNumber(numbers[i]):
+        cnt += 1
+
+print(cnt)
