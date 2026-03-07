726dc02e48a14a1c925543e9d5e8516885191157
w00jinLee <leewoojin001121@gmail.com>
2026-03-08 02:18:22 +0900

    [WEEK2] 파이썬 문법 - 최댓값

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,14 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+
+list = [int(input()) for i in range(9)]
+
+max = 0
+pos = 0
+for i in range(9):
+    if list[i] > max:
+        max = list [i]
+        pos = i+1
+
+print(max, pos)
