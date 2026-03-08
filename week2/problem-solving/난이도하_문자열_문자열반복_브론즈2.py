f301a24d827a33d99b52c508500288a420fe818a
w00jinLee <leewoojin001121@gmail.com>
2026-03-08 14:02:12 +0900

    [WEEK2] 문자열 - 문자열 반복

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,18 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+T = int(input())
+
+
+
+for i in range(T):
+    str=""
+
+    inputs = input().split() # ["반복횟수", "문자열" 로 저장]
+    inputs[0] = int(inputs[0])
+
+    for i in range(len(inputs[1])):
+        for j in range(inputs[0]):
+            str += inputs[1][i]
+
+    print(str)       
