18add5e3b27446e5f462c7572121d89a0f7c3969
w00jinLee <leewoojin001121@gmail.com>
2026-03-11 00:19:29 +0900

    [WEEK2] 재귀함수 - 하노이 탑

--- week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,26 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+
+N = int(input())
+
+
+k=0
+cur=[]
+
+            #원반개수 시작기둥 목표기둥 보조기둥
+def hanoiTop(count, moveFrom, moveTo, sub):
+    global k
+    k+=1
+    #시작,보조,목표
+    if count == 1:
+        print(moveFrom, moveTo)
+        return
+    # 가장 큰 원판을 제외한 모든 원판을 보조 기둥으로 옮겨야함.
+    hanoiTop(count-1, moveFrom, sub, moveTo)
+    print(moveFrom, moveTo)
+    hanoiTop(count-1, sub, moveTo, moveFrom)
+
+
+
+hanoiTop(N, 1, 3, 2)
+print(k)
