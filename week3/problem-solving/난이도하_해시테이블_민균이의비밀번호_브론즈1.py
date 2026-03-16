5d33249a7a7c502a6265af0b82a50ffe621db730
w00jinLee <leewoojin001121@gmail.com>
2026-03-17 01:23:39 +0900

    [WEEK3] 해시 테이블 - 민균이의 비밀번호

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,13 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+
+N = int(input())
+
+wordList = [input() for _ in range(N)]
+
+for i in range(N):
+    reveredWord = wordList[i][::-1]
+
+    if wordList[i] in wordList and reveredWord in wordList  :
+        print(len(wordList[i]), wordList[i][len(wordList[i])//2])
+        break
