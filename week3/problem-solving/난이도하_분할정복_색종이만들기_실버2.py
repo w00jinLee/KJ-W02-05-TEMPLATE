d992e5a51585a0afcdd3c30ed1ff38d3c1244397
w00jinLee <leewoojin001121@gmail.com>
2026-03-17 11:54:38 +0900

    [WEEK3] 분할정복 - 색종이 만들기

--- week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
+++ week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
@@ -1,2 +1,48 @@
 # 분할정복 - 색종이 만들기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/2630
+import sys
+
+N = int(input())
+paper=[]
+for _ in range(N):
+    paper.append(list(map(int, sys.stdin.readline().strip().split())))
+
+
+# 처음에는 N을 기준으로 height, width 
+blueCount = 0
+whiteCount = 0
+bluePaper =False
+whitePaper = False
+
+def cutPaper(y, x, paper_len):
+    global blueCount, whiteCount
+    # 파란색 종이일 때
+    if paper[y][x] == 1:
+        for i in range(y, y + paper_len):
+            for j in range(x, x + paper_len):
+                if paper[i][j] == 0:
+                    cutPaper(y + paper_len//2, x + paper_len//2, paper_len//2)
+                    cutPaper(y + paper_len//2, x, paper_len//2)
+                    cutPaper(y, x + paper_len//2, paper_len//2)
+                    cutPaper(y, x, paper_len//2)
+                    return
+        blueCount +=1
+
+    # 흰색 종이일 때                
+    else : 
+        for i in range(y, y + paper_len):
+            for j in range(x, x + paper_len):
+                if paper[i][j] == 1:
+                    cutPaper(y + paper_len//2, x + paper_len//2, paper_len//2)
+                    cutPaper(y + paper_len//2, x, paper_len//2)
+                    cutPaper(y, x + paper_len//2, paper_len//2)
+                    cutPaper(y, x, paper_len//2)
+                    return
+        whiteCount +=1
+
+
+
+cutPaper(0, 0, N)
+
+print(whiteCount)
+print(blueCount)
