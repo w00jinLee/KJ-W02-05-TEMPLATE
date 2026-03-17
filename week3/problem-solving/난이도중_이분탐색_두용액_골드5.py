470a51ad6198eb3d1a012649397df6b3ce034144
w00jinLee <leewoojin001121@gmail.com>
2026-03-17 22:59:52 +0900

    [WEEK3] 이분탐색 - 두 용액

--- week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
+++ week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
@@ -1,2 +1,32 @@
 # 이분탐색 - 두 용액 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2470
+import sys
+
+N = int(input())
+
+solList = list(map(int, sys.stdin.readline().split()))
+
+#0에 가장 가까운 값, 더한 값에 abs 씌우고 그 중 최솟값 
+solComb = [0, 0]
+solList.sort()
+    # -99 -2 -1 4 98
+
+left = 0
+right = N-1
+minNum = 2000000000
+
+while left < right : 
+    if abs(solList[left] + solList[right]) < minNum :
+        minNum = abs(solList[left] + solList[right])
+        solComb[0] = solList[left]
+        solComb[1] = solList[right]
+    if solList[left] + solList[right] > 0 :
+        right -= 1
+    elif solList[left] + solList[right] == 0 :
+        solComb[0] = solList[left]
+        solComb[1] = solList[right]
+        break
+    else : 
+        left +=1
+
+print(solComb[0], solComb[1])
