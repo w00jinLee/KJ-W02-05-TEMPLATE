0427d0bda805b717aeff6aeb09432f0f63043b4f
w00jinLee <leewoojin001121@gmail.com>
2026-03-20 20:35:33 +0900

    [WEEK4] basic - 트리 - 이진 탐색 트리(BST) 구현

--- week4/basic/02_bst.py
+++ week4/basic/02_bst.py
@@ -51,12 +51,18 @@ def search_bst(root, target):
         True/False
     """
     # TODO: root가 None이면 False 반환
-    pass
-    
+    if root is None:
+        return False
     # TODO: 값을 찾으면 True 반환
     ## target이 작으면 왼쪽 서브트리에서 검색
     ## target이 크면 오른쪽 서브트리에서 검색
-    pass
+    if target == root.value:
+        return True
+    elif target < root.value:
+        return search_bst(root.left, target)
+    else :
+        return search_bst(root.right, target)
+    
 
 # 테스트 케이스
 if __name__ == "__main__":
@@ -79,5 +85,3 @@ if __name__ == "__main__":
     for val in test_values:
         result = search_bst(root, val)
         print(f"값 {val} 검색: {result}")
-
-
