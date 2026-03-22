393f611a4c344969c0efaae97d1615e6feb0069f
w00jinLee <leewoojin001121@gmail.com>
2026-03-22 18:38:49 +0900

    [WEEK4] 트리 - 트리 만들기

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,14 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+n, m = input().split()
+
+n, m = int(n), int(m)
+# n개의 노드, m개의 리프
+
+
+for i in range(n - m) :
+    print(i, i + 1)
+
+for i in range(n - m + 1, n):
+    print(n - m, i)
