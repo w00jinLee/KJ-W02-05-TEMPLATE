801c27d2845fdcf1e53137f263b208ede71bba43
w00jinLee <leewoojin001121@gmail.com>
2026-03-23 17:04:38 +0900

    [WEEK4] 그래프 - 바이러스

--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,31 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+
+computers = int(input())
+edge = int(input())
+
+visited = [False for _ in range(computers+1)]
+
+graph = {}
+
+for i in range(computers+1):
+    graph[i] = []
+
+for _ in range(edge):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+wormCnt =0
+
+def dfs(start):
+    global wormCnt
+    visited[start] = True 
+    wormCnt +=1
+
+    for i in graph[start]:
+        if visited[i] is False:
+            dfs(i)
+dfs(1)
+
+print(wormCnt-1) # 1번 컴퓨터는 제외
