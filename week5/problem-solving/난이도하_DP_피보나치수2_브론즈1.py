35b5fe6d4eceb079444922358cfb98f140c3f891
w00jinLee <leewoojin001121@gmail.com>
2026-03-28 11:06:57 +0900

    [WEEK5] DP - 피보나치 수 2

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,15 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+n = int(input())
+memo=[None] * (n+1)
+memo[0] = 0
+memo[1] = 1
+
+def fib(num):  
+    if memo[num] is not None:
+        return memo[num]
+    memo[num] = fib(num-1) + fib(num-2)
+    return memo[num]
+
+print(fib(n))
