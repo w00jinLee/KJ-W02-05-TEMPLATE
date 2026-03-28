ae93eb88e56678f6a8163ec19f54763ef820cfc3
w00jinLee <leewoojin001121@gmail.com>
2026-03-28 13:10:11 +0900

    [WEEK5] 그리디 - 동전 0

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,18 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+N, K = map(int, input().split())
+coins = []
+for _ in range(int(N)):
+    coins.append(int(input()))
+count =0
+# 큰 동전부터 나누기
+coins.sort(reverse=True)
+
+for coin in coins:
+    if K >= coin:
+        count += K // coin
+        K = K % coin
+
+
+print(count)
