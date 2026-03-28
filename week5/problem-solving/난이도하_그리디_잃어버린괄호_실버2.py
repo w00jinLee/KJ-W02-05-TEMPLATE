112b9877c6f1f9227e03269697dc69b72dcef895
w00jinLee <leewoojin001121@gmail.com>
2026-03-28 15:19:53 +0900

    [WEEK5] 그리디 - 잃어버린 괄호

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,21 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+
+numList = input().split("-")
+result = 0
+
+for i in range(len(numList)):
+    listSum=0
+    if "+" in numList[i]:
+        numList[i] = numList[i].split("+")
+        for j in range(len(numList[i])):
+            listSum += int(numList[i][j])
+        numList[i] = listSum
+    else :
+        numList[i] = int(numList[i])
+
+result += numList[0]
+for i in range(1, len(numList)):
+    result -= numList[i]
+
+print(result)
