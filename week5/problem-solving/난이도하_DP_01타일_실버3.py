a8eb7ff70f768467865c0a4a11afbaec7819032c
w00jinLee <leewoojin001121@gmail.com>
2026-03-28 11:35:40 +0900

    [WEEK5] DP - 01타일

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,14 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+
+N = int(input())
+
+if N == 1:
+    print(1)
+else :
+    dp = [0]*(N+1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, N+1):
+        dp[i] = (dp[i-1] + dp[i-2])%15746
+    print(dp[N])
