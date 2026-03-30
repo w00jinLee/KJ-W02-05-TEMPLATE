cf3cecb8e52f3622a38c309fc43b444641abb6c5
w00jinLee <leewoojin001121@gmail.com>
2026-03-30 22:42:01 +0900

    [WEEK5] DP - 동전

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,19 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input()) # N(1 ≤ N ≤ 20) 
+    coins = list(map(int, input().split())) # 동전의 종류
+    M = int(input()) # 만들어야 할 금액, M(1 ≤ M ≤ 10000)
+
+    dp = [0]*(M+1)
+    dp[0]=1
+
+    # 모든 방법의 수
+    for coin in coins:
+        for i in range(coin, M+1):
+            dp[i] += dp[i-coin]
+
+    print(dp[M])
