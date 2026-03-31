97f7060f25bc28f6c1060fec9f802cfb17dd1162
w00jinLee <leewoojin001121@gmail.com>
2026-03-31 17:50:14 +0900

    [WEEK5] DP - 평범한 배낭

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -1,2 +1,19 @@
 # DP - 평범한 배낭 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/12865
+
+N, K = map(int, input().split())
+
+items = []
+dp = [0] * (K+1)
+
+for _ in range(N):
+    W, V = map(int, input().split())
+    items.append((W,V))
+
+
+for w, v in items:
+    for i in range(K, w-1, -1):
+        dp[i] = max(dp[i] , dp[i-w] + v)
+        
+
+print(dp[-1])
